df9d3f254e6f0a0a415cff68ddd475b1692bb1a3
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-10-28 23:07:27 +0530

    Initila

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.adminLogin');
 });
+
+// Authentication --
+
+
+// Dashboard --
+Route::get('/dashboard', function(){
+    return view('admin.dashboard');
+});
